7453f102405ca9a738e4e359f4a20d047351480d
Colin Finger <finger.colin@gmail.com>
2022-02-19 01:20:06 +0100

    added twcss + some infos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  age = 0;
+
+  ngOnInit() {
+    const birthday = new Date(1996, 4, 25);
+    const time = Math.abs(Date.now() - birthday.getTime());
+    this.age = Math.floor(time / (1000 * 3600 * 24) / 365.25);
+  }
 }
