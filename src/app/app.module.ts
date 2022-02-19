1024b6b9b0d7749b22a945c94f0e2d377e2cc73e
Colin Finger <finger.colin@gmail.com>
2022-02-19 21:54:52 +0100

    intro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { CardComponent } from './card/card.component';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    CardComponent
+    CardComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
