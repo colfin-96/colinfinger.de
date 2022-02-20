7e3caa990045f5130dc7b2c40c10f07fe5693d0f
Colin Finger <finger.colin@gmail.com>
2022-02-20 21:30:24 +0100

    intro animation works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,17 +9,9 @@ import { CardComponent } from './card/card.component';
 import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    CardComponent,
-    BannerComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, CardComponent, BannerComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,15 +1,88 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  animateChild,
+  query,
+  group,
+  AnimationEvent,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.scss']
+  styleUrls: ['./banner.component.scss'],
+  animations: [
+    trigger('initializeBanner', [
+      transition(':enter', [
+        group([
+          animate(
+            '1s cubic-bezier(0.65, 0, 0.35, 1)',
+            style({
+              backgroundColor: 'rgba(28 25 23 / 1)',
+            })
+          ),
+          animate(
+            '0.4s 2.5s cubic-bezier(0.65, 0, 0.35, 1)',
+            style({
+              opacity: 0,
+            })
+          ),
+          query('@innerText', animateChild()),
+          query('@innerText2', animateChild()),
+          query('@line', animateChild()),
+        ]),
+      ]),
+    ]),
+    trigger('innerText', [
+      transition(':enter', [
+        style({
+          opacity: 0,
+        }),
+        animate(
+          '300ms 500ms cubic-bezier(0.65, 0, 0.35, 1)',
+          style({ opacity: 1 })
+        ),
+      ]),
+    ]),
+    trigger('innerText2', [
+      transition(':enter', [
+        style({
+          opacity: 0,
+        }),
+        animate(
+          '300ms 500ms cubic-bezier(0.65, 0, 0.35, 1)',
+          style({ opacity: 1 })
+        ),
+      ]),
+    ]),
+    trigger('line', [
+      transition(':enter', [
+        style({
+          transform: 'scale(0,0)',
+        }),
+        animate(
+          '0.5s 0.5s cubic-bezier(0.65, 0, 0.35, 1)',
+          style({
+            transform: 'scale(1,1)',
+          })
+        ),
+      ]),
+    ]),
+  ],
 })
 export class BannerComponent implements OnInit {
+  componentVisible = true;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onInitializeBannerDone(event: AnimationEvent) {
+    console.log(event);
+    this.componentVisible = false;
   }
-
 }
