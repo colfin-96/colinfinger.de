e291b78df00e24c3b16d00d03f76033f2250c923
Colin Finger <finger.colin@gmail.com>
2022-02-21 22:13:57 +0100

    animation works in banner story

--- src/app/banner/banner.stories.ts
+++ src/app/banner/banner.stories.ts
@@ -1,9 +1,15 @@
-import { Meta, Story } from '@storybook/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Meta, moduleMetadata, Story } from '@storybook/angular';
 import { BannerComponent } from './banner.component';
 
 export default {
   title: 'Sth/Banner',
   component: BannerComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [BrowserAnimationsModule],
+    }),
+  ],
 } as Meta;
 
 export const XYZ: Story = () => ({
