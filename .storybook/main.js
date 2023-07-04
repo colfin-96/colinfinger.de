624c357eb4f95c8d626fc6df84b9c3371dc31401
Colin Finger <finger.colin@gmail.com>
2023-07-04 19:35:50 +0200

    sb update to v7

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,11 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/angular",
-  "core": {
-    "builder": "webpack5"
+  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+  "framework": {
+    name: "@storybook/angular",
+    options: {}
+  },
+  docs: {
+    autodocs: true
   }
-}
+};
